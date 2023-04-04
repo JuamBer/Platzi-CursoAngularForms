9fe6b8c4521c98dd71dd31723949dd78608f2575
Juan Sáez García <juambersaezgarcia@gmail.com>
2023-04-04 19:05:55 +0200

    feat: Cómo usar inputs de texto y la importancia del type

--- src/app/components/basic-form/basic-form.component.ts
+++ src/app/components/basic-form/basic-form.component.ts
@@ -4,22 +4,27 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
-  styleUrls: ['./basic-form.component.scss']
+  styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent implements OnInit {
-
   nameField = new FormControl('');
   emailField = new FormControl('');
   phoneField = new FormControl('');
   colorField = new FormControl('#000000');
   dateField = new FormControl('');
+  monthField = new FormControl('');
   ageField = new FormControl(12);
+  passwordField = new FormControl('');
+  priceField = new FormControl('50');
+  weekField = new FormControl('');
+  timefield = new FormControl('');
+  searchField = new FormControl('');
+  descriptionField = new FormControl('');
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.nameField.valueChanges
-    .subscribe(value => {
+    this.nameField.valueChanges.subscribe((value) => {
       console.log(value);
     });
   }
@@ -27,5 +32,4 @@ export class BasicFormComponent implements OnInit {
   getNameValue() {
     console.log(this.nameField.value);
   }
-
 }
